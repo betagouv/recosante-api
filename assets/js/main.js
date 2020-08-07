c46122bd59a5674e988ca7066335c8c53815873e
David Bruant <davidbruant@protonmail.com>
2020-08-07 12:22:55 +0200

    Rajout de la qualité de l'air dans le csv de sortie

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,46 @@
 
 const INPUT_ALLERGIQUE_COLUMN_NAME = 'Êtes-vous allergique aux pollens (graminées, ambroisie, etc.) ?'
+const INPUT_VILLE_COLUMN_NAME = 'Ville'
+
 const OUTPUT_ALLERGIQUE_COLUMN_NAME = 'Allergique'
+const OUTPUT_QUALITE_AIR_COLUMN_NAME = `Qualité de l'air`
 
 function makeSendingRow(row){
     const sendingRow = {...row};
-    let allergique = row[INPUT_ALLERGIQUE_COLUMN_NAME].trim().slice(0, 3)
+    const TODAY_DATE_STRING = (new Date()).toISOString().slice(0, 10)
 
-    delete sendingRow[INPUT_ALLERGIQUE_COLUMN_NAME]
-    sendingRow[OUTPUT_ALLERGIQUE_COLUMN_NAME] = allergique
+    const ville = sendingRow[INPUT_VILLE_COLUMN_NAME];
+    return d3.json(`https://geo.api.gouv.fr/communes?nom=${ville}&boost=population&limit=1`)
+    .then(geoResult => {
+        const {code: codeINSEE} = geoResult[0];
 
-    return sendingRow
+        return d3.json(`http://app-6ccdcc10-da92-47b1-add6-59d8d3914d79.cleverapps.io/forecast?insee=${codeINSEE}`)
+        .then(apiQAResult => {
+            if(apiQAResult.length === 0)
+                throw new Error('Pas trouvé !')
+
+            return apiQAResult.find(res => res.date === TODAY_DATE_STRING)
+        })
+        .catch(err => {
+            console.warn(`Pas d'information de qualité de l'air pour`, codeINSEE, ville, row, err)
+        })
+    })
+    .catch(err => {
+        console.warn('Code INSEE pour', ville, 'non trouvé', row, err)
+    })
+    .then(indiceATMODate => {
+        //console.log('indiceATMODate', indiceATMODate, ville)
+
+        const {qualif} = indiceATMODate || {}
+        if(qualif)
+            sendingRow[OUTPUT_QUALITE_AIR_COLUMN_NAME] = qualif
+
+        let allergique = sendingRow[INPUT_ALLERGIQUE_COLUMN_NAME].trim().slice(0, 3)
+        delete sendingRow[INPUT_ALLERGIQUE_COLUMN_NAME]
+        sendingRow[OUTPUT_ALLERGIQUE_COLUMN_NAME] = allergique
+
+        return sendingRow;
+    })
 }
 
 
@@ -23,13 +54,12 @@ function makeSendingCSV(file){
         reader.readAsText(file);
     }))
     .then(textContent => {
-        const content = d3.csvParse(textContent)
+        const content = d3.csvParse(textContent)//.slice(0, 10)
 
         console.log('input file', file, content)
 
-        const sendingContent = content.map(makeSendingRow)
-
-        return d3.csvFormat(sendingContent)
+        return Promise.all(content.map(makeSendingRow))
+        .then(sendingContent => d3.csvFormat(sendingContent))
     })
     
 }
