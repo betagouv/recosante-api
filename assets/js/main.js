3430f75ccb8b359e5261042a41125f1a9636274a
David Bruant <davidbruant@protonmail.com>
2020-08-10 12:21:54 +0200

    Production de 4 fichiers en sortie en fonction de la fréquence et du canal

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,6 +37,17 @@ const OUTPUT_PHONE_NUMBER_COLUMN_NAME = `Téléphone`
 const INPUT_FREQUENCY_COLUMN_NAME = `A quelle fréquence souhaitez-vous recevoir les notifications ? `
 const OUTPUT_FREQUENCY_COLUMN_NAME = `Fréquence`
 
+const INPUT_FREQUENCY_EVERYDAY = `tous les jours`
+const INPUT_FREQUENCY_BAD_AIR_QUALITY = `Lorsque la qualité de l'air est mauvaise`
+
+const FREQUENCY_EVERYDAY = `Tous-les-jours`
+const FREQUENCY_BAD_AIR_QUALITY = `Air-mauvais`
+
+const FILENAME_TO_INPUT_FREQ = {
+    [FREQUENCY_EVERYDAY]: INPUT_FREQUENCY_EVERYDAY,
+    [FREQUENCY_BAD_AIR_QUALITY]: INPUT_FREQUENCY_BAD_AIR_QUALITY,
+}
+
 
 const OUTPUT_QUALITE_AIR_COLUMN_NAME = `Qualité de l'air`
 
@@ -79,18 +90,30 @@ function makeSendingRow(row){
         sendingRow[OUTPUT_AUTOMOBILISTE_COLUMN_NAME] = row[INPUT_TRANSPORT_COLUMN_NAME].includes('Voiture') ? OUI : NON;
         sendingRow[OUTPUT_FUMEUR_COLUMN_NAME] = row[INPUT_FUMEUR_COLUMN_NAME].trim()
 
-        const canal = row[INPUT_CANAL_COLUMN_NAME].trim();
-        sendingRow[OUTPUT_CANAL_COLUMN_NAME] = [CANAL_EMAIL, CANAL_SMS].includes(canal) ? canal : '?'
-
         sendingRow[OUTPUT_PHONE_NUMBER_COLUMN_NAME] = row[INPUT_PHONE_NUMBER_COLUMN_NAME].trim()
-        sendingRow[OUTPUT_FREQUENCY_COLUMN_NAME] = row[INPUT_FREQUENCY_COLUMN_NAME].trim()
-        
+                
         return sendingRow;
     })
 }
 
+function makeSendingFileName(freq, canal){
+    const TODAY_DATE_STRING = (new Date()).toISOString().slice(0, 10)
 
-function makeSendingCSV(file){
+    return `${TODAY_DATE_STRING}-${freq}-${canal}.csv`
+}
+
+function makeSendingFileMapEntry(freq, canal, formResponses){
+    return [
+        makeSendingFileName(freq, canal),
+        formResponses.filter(r => 
+            r[INPUT_FREQUENCY_COLUMN_NAME] === FILENAME_TO_INPUT_FREQ[freq] && 
+            r[INPUT_CANAL_COLUMN_NAME] === canal
+        )
+    ]
+}
+
+
+function makeSendingCSVs(file){
     
     return (new Promise( (resolve, reject) => {
         const reader = new FileReader();  
@@ -100,12 +123,24 @@ function makeSendingCSV(file){
         reader.readAsText(file);
     }))
     .then(textContent => {
-        const content = d3.csvParse(textContent)//.slice(0, 10)
-
+        const formResponses = d3.csvParse(textContent, r => {
+            r[INPUT_FREQUENCY_COLUMN_NAME] = r[INPUT_FREQUENCY_COLUMN_NAME].toLowerCase();
+            return r;
+        })//.slice(0, 10)
         //console.log('input file', file, content)
 
-        return Promise.all(content.map(makeSendingRow))
-        .then(sendingContent => d3.csvFormat(sendingContent))
+        const sendingFilesFormResponsesMap = new Map([
+            makeSendingFileMapEntry(FREQUENCY_EVERYDAY, CANAL_EMAIL, formResponses),
+            makeSendingFileMapEntry(FREQUENCY_EVERYDAY, CANAL_SMS, formResponses),
+            makeSendingFileMapEntry(FREQUENCY_BAD_AIR_QUALITY, CANAL_EMAIL, formResponses),
+            makeSendingFileMapEntry(FREQUENCY_BAD_AIR_QUALITY, CANAL_SMS, formResponses),
+        ])
+
+        return Promise.all([...sendingFilesFormResponsesMap].map(([filename, responses]) => {
+            return Promise.all(responses.map(makeSendingRow))
+            .then(sendingRows => [filename, d3.csvFormat(sendingRows)])
+        }))
+        .then(fileEntries => new Map(fileEntries))
     })
     
 }
@@ -118,22 +153,28 @@ document.addEventListener('DOMContentLoaded', e => {
         // replace <input> with list of files
         const file = e.target.files[0];
 
-        const sendingCSVTextP = makeSendingCSV(file)
+        const sendingCSVTextP = makeSendingCSVs(file)
         
-        sendingCSVTextP.then(sendingCSVText => {
-            //console.log('output', sendingCSVText)
+        const ul = document.createElement('ul');
 
-            const name = "fichier de sortie écosanté.csv"
+        sendingCSVTextP.then(sendingCSVMap => {
+            // console.log('output sendingCSVMap', sendingCSVMap)
 
-            const blob = new Blob([sendingCSVText], {type: 'text/csv'});
-            const blobUrl = URL.createObjectURL(blob);
-            const outputFileLink = document.createElement('a');
-            
-            outputFileLink.setAttribute('href', blobUrl);
-            outputFileLink.setAttribute('download', name);
-            outputFileLink.textContent = name;
+            for(const [filename, csvString] of sendingCSVMap){
+                const li = document.createElement('li');
+                const blob = new Blob([csvString], {type: 'text/csv'});
+                const blobUrl = URL.createObjectURL(blob);
+                const outputFileLink = document.createElement('a');
+                
+                outputFileLink.setAttribute('href', blobUrl);
+                outputFileLink.setAttribute('download', filename);
+                outputFileLink.textContent = filename;
 
-            output.append(outputFileLink)
+                li.append(outputFileLink)
+                ul.append(li)
+            }
+
+            output.append(ul)
         })
     })
 
