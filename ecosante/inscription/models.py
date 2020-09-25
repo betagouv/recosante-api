3f8f44b40631706ed1c9d396c0c1b4f7f72962e7
Vincent <lara.vincent@gmail.com>
2020-09-25 10:59:04 +0200

    Fix export csv

--- ecosante/inscription/models.py
+++ ecosante/inscription/models.py
@@ -3,7 +3,7 @@ from sqlalchemy.dialects import postgresql
 from datetime import date
 from dataclasses import dataclass
 from typing import List
-from csv import DictWriter
+import csv
 from io import StringIO
 
 @dataclass
@@ -59,51 +59,54 @@ class Inscription(db.Model):
 
     @classmethod
     def generate_csv(cls):
-        stringio = StringIO()
-        writer = DictWriter(
-            stringio,
-            fieldnames=[
-                'Dans quelle ville vivez-vous ?',
-                'Parmi les choix suivants, quel(s) moyen(s) de transport utilisez-vous principalement pour vos déplacements ?',
-                "Pratiquez-vous une activité sportive au moins une fois par semaine ? On entend par activité sportive toute forme d'activité physique ayant pour objectif l'amélioration et le maintien de la condition physique.",
-                "Pratiquez-vous une Activité Physique Adaptée au moins une fois par semaine ? Les APA regroupent l’ensemble des activités physiques et sportives adaptées aux capacités des personnes atteintes de maladie chronique ou de handicap.",
-                "Pratiquez-vous au moins une fois par semaine les activités suivantes ?",
-                "Vivez-vous avec une pathologie respiratoire ?",
-                "Êtes-vous allergique aux pollens ?",
-                "Êtes-vous fumeur.euse ?",
-                "Vivez-vous avec des enfants ?",
-                "Votre adresse e-mail : elle permettra à l'Equipe Ecosanté de communiquer avec vous si besoin.",
-                "Souhaitez-vous recevoir les recommandations par : *",
-                "Numéro de téléphone :",
-                "A quelle fréquence souhaitez-vous recevoir les recommandations ? *",
-                "Consentez-vous à partager vos données avec l'équipe Écosanté ? Ces données sont stockées sur nextcloud, dans le respect de la réglementation RGPD."
-            ]
-        )
-        writer.writeheader()
-        yield stringio.getvalue()
+        def generate_line(line):
+            stringio = StringIO()
+            writer = csv.writer(stringio)
+            writer.writerow(line)
+            v = stringio.getvalue()
+            stringio.close()
+            return v
+
+        yield generate_line(['Dans quelle ville vivez-vous ?',
+            'Parmi les choix suivants, quel(s) moyen(s) de transport utilisez-vous principalement pour vos déplacements ?',
+            "Pratiquez-vous une activité sportive au moins une fois par semaine ? On entend par activité sportive toute forme d'activité physique ayant pour objectif l'amélioration et le maintien de la condition physique.",
+            "Pratiquez-vous une Activité Physique Adaptée au moins une fois par semaine ? Les APA regroupent l’ensemble des activités physiques et sportives adaptées aux capacités des personnes atteintes de maladie chronique ou de handicap.",
+            "Pratiquez-vous au moins une fois par semaine les activités suivantes ?",
+            "Vivez-vous avec une pathologie respiratoire ?",
+            "Êtes-vous allergique aux pollens ?",
+            "Êtes-vous fumeur.euse ?",
+            "Vivez-vous avec des enfants ?",
+            "Votre adresse e-mail : elle permettra à l'Equipe Ecosanté de communiquer avec vous si besoin.",
+            "Souhaitez-vous recevoir les recommandations par : *",
+            "Numéro de téléphone :",
+            "A quelle fréquence souhaitez-vous recevoir les recommandations ? *",
+            "Consentez-vous à partager vos données avec l'équipe Écosanté ? Ces données sont stockées sur nextcloud, dans le respect de la réglementation RGPD."
+        ])
+
         for inscription in Inscription.query.all():
-            stringio.truncate(0)
             diffusion = inscription.diffusion
             if diffusion == 'mail':
                 diffusion = 'Mail'
             elif diffusion == 'sms':
                 diffusion = 'SMS'
 
-            writer.writerow({
-                'Dans quelle ville vivez-vous ?': inscription.ville_entree,
-                'Parmi les choix suivants, quel(s) moyen(s) de transport utilisez-vous principalement pour vos déplacements ?': "; ".join(inscription.deplacement or []),
-                "Pratiquez-vous une activité sportive au moins une fois par semaine ? On entend par activité sportive toute forme d'activité physique ayant pour objectif l'amélioration et le maintien de la condition physique.": inscription.activites is not None and 'sport' in inscription.activites,
-                "Pratiquez-vous une Activité Physique Adaptée au moins une fois par semaine ? Les APA regroupent l’ensemble des activités physiques et sportives adaptées aux capacités des personnes atteintes de maladie chronique ou de handicap.": False,
-                "Pratiquez-vous au moins une fois par semaine les activités suivantes ?": "; ".join(inscription.activites or []),
-                "Vivez-vous avec une pathologie respiratoire ?": inscription.pathologie_respiratoire,
-                "Êtes-vous allergique aux pollens ?": inscription.allergie_pollen,
-                "Êtes-vous fumeur.euse ?": inscription.fumeur,
-                "Vivez-vous avec des enfants ?": inscription.enfants,
-                "Votre adresse e-mail : elle permettra à l'Equipe Ecosanté de communiquer avec vous si besoin.": inscription.mail,
-                "Souhaitez-vous recevoir les recommandations par : *": diffusion,
-                "Numéro de téléphone :": inscription.telephone,
-                "A quelle fréquence souhaitez-vous recevoir les recommandations ? *": inscription.frequence,
-                "Consentez-vous à partager vos données avec l'équipe Écosanté ? Ces données sont stockées sur nextcloud, dans le respect de la réglementation RGPD.": True
-            })
-            yield stringio.getvalue()
-        stringio.close()
+            yield generate_line([
+                inscription.ville_entree,
+                "; ".join(inscription.deplacement or []),
+                cls.convert_boolean_to_oui_non(inscription.activites is not None and 'sport' in inscription.activites),
+                "Non",
+                ";".join(inscription.activites or []),
+                cls.convert_boolean_to_oui_non(inscription.pathologie_respiratoire),
+                cls.convert_boolean_to_oui_non(inscription.allergie_pollen),
+                cls.convert_boolean_to_oui_non(inscription.fumeur),
+                cls.convert_boolean_to_oui_non(inscription.enfants),
+                inscription.mail,
+                diffusion,
+                inscription.telephone,
+                inscription.frequence,
+                "Oui"
+            ])
+
+    @classmethod
+    def convert_boolean_to_oui_non(cls, value):
+        return "Oui" if value else "Non"
