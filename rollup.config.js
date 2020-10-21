e6573e638894b40a53d6695772f9766cb93ce849
Vincent <lara.vincent@gmail.com>
2020-10-21 16:38:09 +0200

    Better rollupconfig

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,14 @@ import postcss from 'rollup-plugin-postcss'
 import copy from 'rollup-plugin-copy'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 
-export default [
+const config_css = [
     {
         input: 'assets/css/style.scss',
         output: {
-            file: 'public/assets/css/style.css',
+            dir: 'public/assets/css/',
+            entryFileNames: '[name].css'
         },
         plugins: [
             postcss({extract: true}),
@@ -17,24 +19,30 @@ export default [
                 ]
             }),
         ]
-    }, {
-        input: 'assets/js/stats.js',
-        output: {
-            file: 'public/assets/js/stats.js',
-            format: 'iife'
-        },
-        plugins: [
-            resolve(),
-            commonjs(),
-            postcss(),
-        ]
-    }, {
-        input: 'assets/js/recommz.js',
-        output: {
-            file: 'public/assets/js/recommz.js'
-        },
-        plugins: [
-            postcss()
-        ]
     }
 ];
+
+const config_js = ['assets/js/recommz.js', 'assets/js/stats.js'].map(
+    filename => {
+        return {
+            input: filename,
+            output: {
+                entryFileNames: '[name].js',
+                dir: 'public/assets/js/',
+                sourcemap: true,
+                format: 'iife',
+            },
+            plugins: [
+                commonjs(),
+                postcss(),
+                babel({
+                    babelHelpers: 'bundled',
+                    exclude: 'node_modules/**'
+                }),
+                resolve(),
+            ],
+
+        }
+    }
+)
+export default config_css.concat(config_js)
