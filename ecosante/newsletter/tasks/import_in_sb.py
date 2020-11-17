ddbbc65b5db08741eb6aefb3486e20b53243f4ae
Vincent <lara.vincent@gmail.com>
2020-11-17 10:36:24 +0100

    use quote_plus to encore mail

--- ecosante/newsletter/tasks/import_in_sb.py
+++ ecosante/newsletter/tasks/import_in_sb.py
@@ -4,7 +4,7 @@ import csv
 import os
 import sib_api_v3_sdk
 from sib_api_v3_sdk.rest import ApiException
-from urllib.parse import quote, urlencode
+from urllib.parse import quote_plus
 from ecosante.newsletter.models import Newsletter, NewsletterDB
 from ecosante.extensions import db, sib, celery
 
@@ -132,7 +132,7 @@ def import_(task, newsletters, overhead=0):
     contact_api = sib_api_v3_sdk.ContactsApi(sib)
     for i, nl in enumerate(newsletters):
         contact_api.update_contact(
-            urlencode(nl.inscription.mail),
+            quote_plus(nl.inscription.mail),
             sib_api_v3_sdk.UpdateContact(
                 attributes=nl.attributes(),
                 list_ids=[lists[nl.inscription.diffusion]]
