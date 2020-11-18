3f87c225cac17f30b065549f5f8f71a422c7ef28
Vincent <lara.vincent@gmail.com>
2020-11-18 12:38:43 +0100

    Ajout page d'admin

--- ecosante/pages/blueprint.py
+++ ecosante/pages/blueprint.py
@@ -2,6 +2,7 @@ from flask import (
     render_template
 )
 from ecosante.utils import Blueprint
+from ecosante.utils.decorators import admin_capability_url
 
 bp = Blueprint("pages", __name__, url_prefix='/')
 
@@ -12,3 +13,9 @@ def index():
 @bp.route('/donnees-personnelles')
 def donnees_personnelles():
     return render_template("donnees-personnelles.html")
+
+@bp.route('/admin/<secret_slug>')
+@admin_capability_url
+def admin(secret_slug):
+    return render_template("admin.html", secret_slug=secret_slug)
+
