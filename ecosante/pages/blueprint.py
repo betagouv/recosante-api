380618e7de33c4da7ee12c760530cb5686b25763
Vincent <lara.vincent@gmail.com>
2020-11-19 15:23:52 +0100

    Ajout du nombre d'avis sur la page d'admin

--- ecosante/pages/blueprint.py
+++ ecosante/pages/blueprint.py
@@ -3,6 +3,8 @@ from flask import (
 )
 from ecosante.utils import Blueprint
 from ecosante.utils.decorators import admin_capability_url
+from datetime import date, timedelta
+from ecosante.newsletter.models import NewsletterDB
 
 bp = Blueprint("pages", __name__, url_prefix='/')
 
@@ -17,4 +19,9 @@ def donnees_personnelles():
 @bp.route('/admin/<secret_slug>')
 @admin_capability_url
 def admin(secret_slug):
-    return render_template("admin.html", secret_slug=secret_slug)
+    count_avis = NewsletterDB.query\
+        .filter(
+            NewsletterDB.avis.isnot(None),
+            NewsletterDB.date==date.today() - timedelta(days=1))\
+        .count()
+    return render_template("admin.html", secret_slug=secret_slug, count_avis=count_avis)
