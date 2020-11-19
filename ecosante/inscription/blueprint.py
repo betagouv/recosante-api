0bc584d426b1aaf7b27435b837b81641b9bad924
Vincent <lara.vincent@gmail.com>
2020-11-19 16:55:39 +0100

    mmore logs on webhook

--- ecosante/inscription/blueprint.py
+++ ecosante/inscription/blueprint.py
@@ -76,6 +76,8 @@ def import_():
 @bp.route('<secret_slug>/user_unsubscription', methods=['POST'])
 @webhook_capability_url
 def user_unsubscription(secret_slug):
+    current_app.logger.error(f"user_unsubscription: {request.data}")
+    current_app.logger.error(f"user_unsubscription: {request.content_type}")
     current_app.logger.error(f"user_unsubscription: {request.json}")
     mail = request.json['email']
     user = Inscription.query.filter_by(mail=mail).first()
