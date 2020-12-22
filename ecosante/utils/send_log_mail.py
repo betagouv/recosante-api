56c85aab49a47026d8f87de79a7f9d90667495a0
Vincent <lara.vincent@gmail.com>
2020-12-22 14:24:51 +0100

    Fix send_log_mail

--- ecosante/utils/send_log_mail.py
+++ ecosante/utils/send_log_mail.py
@@ -1,27 +1,23 @@
-import os
-import requests
+from ecosante.extensions import sib
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
+from flask import current_app
 
 def send_log_mail(subject, text_content):
-    sib_apikey = os.getenv('SIB_APIKEY')
-    r = requests.post(
-        'https://api.sendinblue.com/v3/smtp/email',
-        headers={
-            'accept': 'application/json',
-            'api-key': sib_apikey
-        },
-        json={
-            "sender": {
-                "name":"L'équipe écosanté",
-                "email":"ecosante@data.gouv.fr"
-            },
-            "to": [{
-                    "email": "ecosante@data.gouv.fr"
-            }],
-            "replyTo": {
-                "name":"L'équipe écosanté",
-                "email":"ecosante@data.gouv.fr"
-            },
-            "subject": subject,
-            "textContent": text_content
-        }
+    contact_dict = {
+        "name": "equipe ecosante",
+        "email": 'ecosante@data.gouv.fr'
+    }
+    api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib)
+    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
+        sender=sib_api_v3_sdk.SendSmtpEmailSender(**contact_dict),
+        to=[sib_api_v3_sdk.SendSmtpEmailTo(**contact_dict)],
+        reply_to=sib_api_v3_sdk.SendSmtpEmailReplyTo(**contact_dict),
+        subject=subject,
+        text_content=text_content
     )
+    try:
+        api_response = api_instance.send_transac_email(send_smtp_email)
+        current_app.logger.debug(api_response)
+    except ApiException as e:
+        current_app.logger.error("Exception when calling TransactionalEmailsApi->send_transac_email: %s\n" % e)
