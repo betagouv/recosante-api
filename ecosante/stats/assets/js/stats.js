a76f9b50b8e4876f51370b074c99d0c9016080ae
Vincent <lara.vincent@gmail.com>
2021-02-18 14:56:50 +0100

    Better stats on mobile

--- ecosante/stats/assets/js/stats.js
+++ ecosante/stats/assets/js/stats.js
@@ -16,32 +16,31 @@ const makeChartData = (data_id, style, label) => {
 }
 
 const new_chart = (elem_id, data_id, title, type, style, label, other_options) => {
-    const ChartData = makeChartData(data_id, style, label)
-    var ctx_format = document.getElementById(elem_id).getContext('2d');
-    window.charts.push(
-        new Chart(ctx_format, {
-            type: type,
-            data: ChartData,
-            options: {
-                ...{
-                    responsive: true,
-                    title: {
-                        display: true,
-                        text: title,
-                        verticalAlign: "center",
-                        dockInsidePlotArea: true
-                    }
-                },
-                ...other_options
+    let data = makeChartData(data_id, style, label)
+    let ctx_format = document.getElementById(elem_id).getContext('2d');
+    let options = {
+        ...{
+            responsive: true,
+            maintainAspectRatio: window.innerWidth > 565,
+            title: {
+                display: true,
+                text: title,
+                verticalAlign: "center",
+                dockInsidePlotArea: true
             }
-        })
-    )
+        },
+        ...other_options
+    }
+    console.log(options)
+    window.charts.push(new Chart(ctx_format, {data, options, type}))
 }
 
 const new_doughnut = (elem_id, data_id, title) => {
     const backgroundColor = [
         '#ffa725',
-        '#2fa0f2'
+        '#2fa0f2',
+        'rgb(75, 192, 192)',
+        'rgb(255, 205, 86)'
     ]
     new_chart(elem_id, data_id, title, 'doughnut', {backgroundColor: backgroundColor}, 'data')
 }
@@ -70,11 +69,10 @@ document.addEventListener('DOMContentLoaded', e => {
     new_bar_chart('subscriptions_chart', 'subscriptions', '# inscrits')
     new_doughnut('user_formats', "media", 'Média')
     new_doughnut('user_frequencies', "frequence", 'Fréquence')
-    new_bar_chart(
+    new_doughnut(
         'decouverte_chart',
         'decouverte',
         "Avis sur les recommandations reçues dans les bulletins d'information Ecosanté (sur la base des réponses au questionnaire de satisfaction)",
-        "horizontalBar"
     )
     new_bar_chart(
         'ouvertures_chart',
