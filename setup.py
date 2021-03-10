3e30885f9ccdffaa0b72a4370c1c5182b97532ab
Vincent <lara.vincent@gmail.com>
2021-03-10 08:23:57 +0100

    bump indice_pollution version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ DEPENDENCIES = [
     'Flask-Migrate',
     'Flask-SQLAlchemy',
     'Flask-WTF',
-    'indice_pollution==0.7.7',
+    'indice_pollution==0.7.8',
     'sib-api-v3-sdk',
     'requests',
     'icalevents'
