0fecc8b58eef7bd4b73b47298b227e9809334650
Vincent <lara.vincent@gmail.com>
2021-03-18 16:58:32 +0100

    Add sentry to celery

--- ecosante/celery_worker.py
+++ ecosante/celery_worker.py
@@ -1,5 +1,14 @@
 from . import create_app
+import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
+import os
 
+if os.getenv('SENTRY_DSN'):
+    sentry_sdk.init(
+        dsn=os.getenv('SENTRY_DSN'),
+        integrations=[CeleryIntegration()],
+        traces_sample_rate=1.0
+    )
 app = create_app()
 
 from .extensions import celery #noqa
