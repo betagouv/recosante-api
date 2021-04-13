da071eb80e0de872861640d6cfa049f74b3e744a
Vincent <lara.vincent@gmail.com>
2021-04-13 17:09:15 +0200

    Add deactivate accounts task

--- ecosante/inscription/tasks/__init__.py
+++ ecosante/inscription/tasks/__init__.py
@@ -1,2 +1,17 @@
 from .send_success_email import send_success_email #noqa
 from .send_unsubscribe import send_unsubscribe, send_unsubscribe_error #noqa
+
+from ecosante.extensions import celery
+from ecosante.inscription.models import Inscription
+from celery.schedules import crontab
+
+@celery.task
+def deactivate_accounts():
+    Inscription.deactivate_accounts()
+
+@celery.on_after_configure.connect
+def setup_periodic_inscriptions_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(minute='0', hour='6', day_of_week='*/1'),
+        deactivate_accounts.s()
+    )
