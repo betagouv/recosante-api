56e6aaf3ab9b120ca2f8b1a737c949b4ca90750a
Vincent <lara.vincent@gmail.com>
2021-04-15 11:43:02 +0200

    remove dead code

--- ecosante/newsletter/blueprint.py
+++ ecosante/newsletter/blueprint.py
@@ -5,28 +5,14 @@ from flask import (
     redirect,
     url_for,
     stream_with_context,
-    session
 )
 from flask.wrappers import Response
-from datetime import date, datetime
-import json
-import os
-from time import time
-from uuid import uuid4
+from datetime import datetime
 
-from werkzeug.urls import url_encode
-from indice_pollution.regions.solvers import get_region
-from indice_pollution.history.models import IndiceHistory
-from ecosante.recommandations.models import Recommandation, db
-from ecosante.utils.decorators import admin_capability_url, task_status_capability_url
+from ecosante.utils.decorators import admin_capability_url
 from ecosante.utils import Blueprint
-from ecosante.extensions import celery
 from .forms import FormAvis
-from .models import (
-    Newsletter,
-    NewsletterDB,
-    Recommandation
-)
+from .models import NewsletterDB, db
 
 bp = Blueprint("newsletter", __name__)
 
