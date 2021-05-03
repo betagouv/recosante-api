d789c08e522240bb4462e0f4de3b7ef3f974ac8e
Vincent <lara.vincent@gmail.com>
2021-05-03 17:57:30 +0200

    Fix indice_pollution

--- ecosante/newsletter/models.py
+++ ecosante/newsletter/models.py
@@ -171,8 +171,8 @@ class Newsletter:
                 recommandations=recommandations,
                 forecast=insee_forecast[inscription.ville_insee].get("forecast"),
                 episodes=insee_forecast[inscription.ville_insee].get("episode"),
-                raep=insee_forecast[inscription.ville_insee].get("raep"),
-                allergenes=insee_forecast[inscription.ville_insee].get("allergenes")
+                raep=insee_forecast[inscription.ville_insee].get("raep", {}).get("total"),
+                allergenes=insee_forecast[inscription.ville_insee].get("raep", {}).get("liste_allergenes")
             )
             if inscription.frequence == "pollution" and newsletter.qualif and newsletter.qualif not in ['mauvais', 'tres_mauvais', 'extrement_mauvais']:
                 continue
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ DEPENDENCIES = [
     'Flask-SQLAlchemy',
     'Flask-WTF',
     'wtforms[email]',
-    'indice_pollution==0.12.0',
+    'indice_pollution==0.12.1',
     'sib-api-v3-sdk',
     'requests',
     'icalevents',
