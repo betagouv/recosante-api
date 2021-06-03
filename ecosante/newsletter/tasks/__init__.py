f50e6b5599307b8e69b5538bf417c8a79312404d
Vincent <lara.vincent@gmail.com>
2021-06-03 09:08:39 +0200

    Remove send indices temporaly

--- ecosante/newsletter/tasks/__init__.py
+++ ecosante/newsletter/tasks/__init__.py
@@ -13,25 +13,15 @@ def save_indice():
         current_app.logger.info(f'Save history for {nl.inscription.ville_insee}')
     db.session.commit()
 
-@celery.task()
-def save_all_indices():
-    save_all()
-
 
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(
-        crontab(minute='0', hour='*/1'),
-        save_indice.s(),
-        queue='save_indices',
-        routing_key='save_indices.nl_export'
-    )
-    sender.add_periodic_task(
-        crontab(minute='0', hour='*/1'),
-        save_all_indices.s(),
-        queue='save_indices',
-        routing_key='save_indices.save_all'
-    )
+    #sender.add_periodic_task(
+    #    crontab(minute='0', hour='*/1'),
+    #    save_indice.s(),
+    #    queue='save_indices',
+    #    routing_key='save_indices.nl_export'
+    #)
     sender.add_periodic_task(
         crontab(minute='0', hour='5', day_of_week='*/1'),
         import_send_and_report.s(),
