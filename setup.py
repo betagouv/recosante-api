88a1d34de2b0df1f21bb1c4e6112daf25faa5da9
Vincent <lara.vincent@gmail.com>
2021-06-07 20:57:07 +0200

    Bump indice_pollution

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ DEPENDENCIES = [
     'Flask-SQLAlchemy',
     'Flask-WTF',
     'wtforms[email]',
-    'indice_pollution==0.15.5',
+    'indice_pollution==0.16.0',
     'sib-api-v3-sdk',
     'requests',
     'icalevents',
