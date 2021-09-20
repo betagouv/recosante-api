f7aff21d334939e5434f934bdac4e2fb027a7dac
Vincent <lara.vincent@gmail.com>
2021-09-20 10:35:45 +0200

    Change queues

--- ecosante/__init__.py
+++ ecosante/__init__.py
@@ -23,13 +23,14 @@ def configure_celery(flask_app):
     }
     celery.conf.update(celery_conf)
     celery.conf.env = flask_app.config['ENV']
-    queues = [
-        Queue("default", routing_key='task.#'),
-        Queue("send_newsletter", routing_key='send_newsletter.#'),
-        Queue("send_email", routing_key='send_email.#'),
-    ]
     if flask_app.config['ENV'] == 'staging':
-        queues.append(Queue("staging"), routing_key='staging.#')
+        queues = [Queue("staging", routing_key='staging.#')]
+    else:
+	    queues = [
+            Queue("default", routing_key='task.#'),
+            Queue("send_newsletter", routing_key='send_newsletter.#'),
+            Queue("send_email", routing_key='send_email.#'),
+        ]
     celery.conf.task_queues = tuple(queues)
     celery.conf.task_default_exchange = 'tasks'
     celery.conf.task_default_exchange_type = 'topic'
