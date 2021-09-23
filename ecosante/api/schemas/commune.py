13fa22868e2a1e26c5dd50d173c5939a3c3ebce8
Vincent <lara.vincent@gmail.com>
2021-09-23 16:17:11 +0200

    Fix codes_postaux

--- ecosante/api/schemas/commune.py
+++ ecosante/api/schemas/commune.py
@@ -7,6 +7,6 @@ class DepartementSchema(Schema):
 class CommuneSchema(Schema):
     code = fields.String()
     nom = fields.String()
-    codesPostaux = fields.List(fields.String())
+    codesPostaux = fields.List(fields.String(), attribute='codes_postaux')
     departement = fields.Nested(DepartementSchema)
 
