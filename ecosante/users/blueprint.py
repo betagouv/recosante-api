05871e88f0485fd2b60f1d8896cf3539233c19ff
Vincent <lara.vincent@gmail.com>
2021-10-06 18:09:54 +0200

    Ajout d’une fonction de recherche d’utilisateurs

--- ecosante/users/blueprint.py
+++ ecosante/users/blueprint.py
@@ -1,9 +1,26 @@
+from flask.globals import request
 from ecosante.extensions import rebar, db
+from ecosante.utils.decorators import admin_capability_url
 from .schemas import RequestPOST, Response, RequestPOSTID
 from ecosante.inscription.models import Inscription
+from flask import session, current_app, abort
+from marshmallow.fields import List
 import os
 
 registry = rebar.create_handler_registry('/users/')
+
+@registry.handles(
+    rule='/_search',
+    hidden=True,
+    response_body_schema={200:Response(many=True)}
+)
+@admin_capability_url
+def search_users():
+    mail = request.args.get('mail')
+    return Inscription.active_query()\
+        .filter(Inscription.mail.ilike(f'%{mail}%'))\
+        .order_by(Inscription.mail)\
+        .limit(10)
 
 @registry.handles(
     rule='/',
