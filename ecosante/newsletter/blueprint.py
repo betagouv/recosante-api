7e6b1101edb168eeb004523cf426dfb27d67d861
Vincent <lara.vincent@gmail.com>
2021-10-12 17:02:36 +0200

    Ajout envoie des notifications dans le test de la newsletter

--- ecosante/newsletter/blueprint.py
+++ ecosante/newsletter/blueprint.py
@@ -18,6 +18,7 @@ from ecosante.utils import Blueprint
 from .forms import FormAvis
 from .models import Newsletter, NewsletterDB, db
 from .tasks.import_in_sb import import_, send
+from .tasks.send_webpush_notifications import send_webpush_notification, vapid_claims
 from indice_pollution.history.models import IndiceATMO
 
 bp = Blueprint("newsletter", __name__)
@@ -98,16 +99,28 @@ def test():
     indice_atmo = int(request.form.get("indice_atmo"))
     uid = request.form.get("uid")
     inscription = Inscription.query.filter_by(uid=uid).first()
-    nl = Newsletter(
-        inscription=inscription,
-        forecast={"data":[{"date": str(today()), "label": IndiceATMO.label_from_valeur(indice_atmo), "couleur": IndiceATMO.couleur_from_valeur(indice_atmo)}]},
-        raep=request.form.get("raep"),
-        allergenes={k: v for k, v in zip(request.form.getlist('allergene_nom'), request.form.getlist('allergene_value'))},
-        validite_raep={
-            "debut": today().strftime("%d/%m/%Y"),
-            "fin": (today()+timedelta(days=7)).strftime("%d/%m/%Y")
-        }
-    )
-    result = import_(None, [NewsletterDB(nl)], force_send=True, test=True)
-    send(None, result["email_campaign_id"], test=True)
-    return render_template("test_ok.html")
+    nb_mails = 0
+    nb_notifications = 0
+    for media in inscription.indicateurs_media:
+        nl = Newsletter(
+            inscription=inscription,
+            forecast={"data":[{"date": str(today()), "label": IndiceATMO.label_from_valeur(indice_atmo), "couleur": IndiceATMO.couleur_from_valeur(indice_atmo)}]},
+            raep=request.form.get("raep"),
+            allergenes={k: v for k, v in zip(request.form.getlist('allergene_nom'), request.form.getlist('allergene_value'))},
+            validite_raep={
+                "debut": today().strftime("%d/%m/%Y"),
+                "fin": (today()+timedelta(days=7)).strftime("%d/%m/%Y")
+            }
+        )
+        if media == "mail":
+            result = import_(None, [NewsletterDB(nl)], force_send=True, test=True)
+            send(None, result["email_campaign_id"], test=True)
+            nb_mails += 1
+        elif media == "notifications_web":
+            for wp in inscription.webpush_subscriptions_info:
+                nl.webpush_subscription_info = wp
+                nl.webpush_subscription_info_id = wp.id
+                send_webpush_notification(NewsletterDB(nl), vapid_claims)
+                nb_notifications += 1
+
+    return render_template("test_ok.html", nb_mails=nb_mails, nb_notifications=nb_notifications)
