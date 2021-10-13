31fc16344cd9cfd036b23da59181ecd07216a23c
Vincent <lara.vincent@gmail.com>
2021-10-13 14:35:34 +0200

    Bump indice_pollution

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ DEPENDENCIES = [
     'Flask-Manage-Webpack',
     'html2text', # to convert from HTML to markdown, used by one alembic revision
     'wtforms[email]',
-    'indice_pollution==0.20.1',
+    'indice_pollution==0.20.2',
     'markdown',
     'markdown-link-attr-modifier',
     'sib-api-v3-sdk',
