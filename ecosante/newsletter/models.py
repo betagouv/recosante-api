08c8187b33c934dd091ff8f4ef8b49b9043c4b41
Vincent <lara.vincent@gmail.com>
2021-11-03 16:14:53 +0100

    Fix newsletter

--- ecosante/newsletter/models.py
+++ ecosante/newsletter/models.py
@@ -209,7 +209,7 @@ class Newsletter:
             .filter(Recommandation.status == "published")\
             .order_by(text("nl.date nulls first"), Recommandation.ordre)
 
-    def eligible_recommandations(self, recommandations: Dict[Recommandation], types=["generale", "episode_pollution", "pollens"], media="newsletter_quotidienne"):
+    def eligible_recommandations(self, recommandations: Dict[int, Recommandation], types=["generale", "episode_pollution", "pollens"], media="newsletter_quotidienne"):
         if not recommandations:
             return
             yield # See https://stackoverflow.com/questions/13243766/python-empty-generator-function
