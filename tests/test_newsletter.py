87df50d4836ea6d0704aa2027f36d27cfbfed298
Vincent <lara.vincent@gmail.com>
2021-12-07 19:10:48 +0100

    Fix tests

--- tests/test_newsletter.py
+++ tests/test_newsletter.py
@@ -534,6 +534,7 @@ def test_export_simple(db_session, inscription):
     assert len(newsletters) == 1
 
 def test_export_user_hebdo(db_session, inscription, templates):
+    inscription.recommandations_actives = ["non"]
     newsletters = list(Newsletter.export(type_='hebdomadaire'))
     assert len(newsletters) == 0
 
