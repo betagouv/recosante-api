102a016df7169436d21e8fa362a37b9fad6e8706
Vincent <lara.vincent@gmail.com>
2021-12-08 17:39:43 +0100

    Fix inactive region for episode de pollution

--- ecosante/api/schemas/episode_pollution.py
+++ ecosante/api/schemas/episode_pollution.py
@@ -52,17 +52,22 @@ class EpisodePollutionSchema(FullIndiceSchema):
                    "url": region.aasqa_website
                 } for region in regions
         ]
-        if data["indice"]:
-            start = min([e.date_ech for e in data["indice"]])
-            end = max([e.date_ech for e in data["indice"]]) + timedelta(1) - timedelta(seconds=1)
-            area = data["indice"][0].zone.lib if data["indice"] else ""
+        errors = [e.error for e in data["indice"] if hasattr(e, "error")]
+        if len(errors) > 0:
+            data['error'] = ",".join(errors)
+            del data["indice"]
         else:
-            start = None
-            end = None
-            area = None
-        data['validity'] = {
-            "start": start,
-            "end": end,
-            "area": area
-        }
+            if data["indice"]:
+                start = min([e.date_ech for e in data["indice"]])
+                end = max([e.date_ech for e in data["indice"]]) + timedelta(1) - timedelta(seconds=1)
+                area = data["indice"][0].zone.lib if data["indice"] else ""
+            else:
+                start = None
+                end = None
+                area = None
+            data['validity'] = {
+                "start": start,
+                "end": end,
+                "area": area
+            }
         return data
