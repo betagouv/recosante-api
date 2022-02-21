3490540226f2fc57e3a47b53e365b9d0fd624105
Vincent Lara <lara.vincent@gmail.com>
2022-02-21 16:38:41 +0100

    Suppression de tests inutilisés

--- tests/test_inscription.py
+++ tests/test_inscription.py
@@ -14,46 +14,6 @@ def data_tester(response, data):
     for k, v in data.items():
         assert response.json[k] == v
 
-def test_inscription_multi_etapes(client):
-    mail, uid = premiere_etape(client)
-
-    data = {
-        'ville_insee': '53130',
-        'deplacement': ['velo', 'tec'],
-        'activites': ['jardinage'],
-        'population': ['pathologie_respiratoire', 'allergie_pollens']
-    }
-    response = client.post(f'/inscription/{uid}/', data=data)
-    data_tester(response, data)
-
-    inscription = Inscription.query.filter_by(uid=uid).first()
-
-    assert inscription
-    assert inscription.mail == mail
-    assert inscription.diffusion == 'mail'
-    assert inscription.frequence == 'quotidien'
-    assert inscription.ville_insee == '53130'
-    assert inscription.deplacement == ['velo', 'tec']
-    assert inscription.activites == ['jardinage']
-
-def test_errors(client):
-    _mail, uid = premiere_etape(client)
-
-    response = client.post(f'/inscription/{uid}/', data={"ville_insee": "13"}, headers={"Accept": "application/json"})
-    assert response.status_code == 400
-    assert 'ville_insee' in response.json
-
-def test_changement_ville(client):
-    _mail, uid = premiere_etape(client)
-    response = client.post(f'/inscription/{uid}/', json={"ville_insee": "53130"})
-    assert response.status_code == 200
-    assert response.json['ville_nom'] == 'Laval'
-
-    response = client.post(f'/inscription/{uid}/', json={"ville_insee": "53144"})
-    assert response.status_code == 200
-    assert response.json['ville_nom'] == 'Marcillé-la-Ville'
-
-
 def test_query_inactive_accounts(db_session):
     i = Inscription(mail='test@test.com')
     db_session.add(i)
@@ -85,20 +45,6 @@ def test_deactivate_accounts(db_session):
     assert db_session.query(Inscription).filter(Inscription.mail==None).count() == 1
     assert Inscription.deactivate_accounts() == 1
     assert db_session.query(Inscription).filter(Inscription.mail==None).count() == 2
-
-
-def test_partial_update_same_mail(db_session, client):
-    i = Inscription(mail='test@test.com')
-    db_session.add(i)
-    db_session.commit()
-
-    mail, uid = premiere_etape(client)
-
-    data = {
-        'mail': 'test@test.com',
-    }
-    response = client.post(f'/inscription/{uid}/', data=data)
-    assert response.status_code == 400
 
 
 def test_add_webpush_subscriptions_info_bad_json(inscription):
