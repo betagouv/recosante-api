8dc3120489d0a47452959fa11f0553af25c4cf56
fcoufour <github@coufourier.com>
2022-03-07 11:57:02 +0100

    Bump indice_pollution version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ DEPENDENCIES = [
     'Flask-Manage-Webpack',
     'html2text', # to convert from HTML to markdown, used by one alembic revision
     'wtforms[email]',
-    'indice_pollution==0.30.0',
+    'indice_pollution==0.30.1',
     'markdown',
     'markdown-link-attr-modifier',
     'openpyxl',
