6490809eba731fdd96c41c56df4f24634e6a972c
Vincent Lara <lara.vincent@gmail.com>
2022-03-30 15:11:29 +0200

    Ajout de la charnière et de l’article dans la commune et le département
    Mais seulement dans ce qui est envoyé dans l’objet commune.
    J’aurai voulu ajouter ça aussi dans area, mais ça casserait l’API.

--- ecosante/api/schemas/commune.py
+++ ecosante/api/schemas/commune.py
@@ -5,12 +5,16 @@ from ecosante.extensions import db
 class DepartementSchema(Schema):
     code = fields.String()
     nom = fields.String()
+    charniere = fields.String(dump_only=True, allow_none=True)
+    article = fields.String(dump_only=True, allow_none=True)
 
 class CommuneSchema(Schema):
     code = fields.String()
     nom = fields.String(dump_only=True, allow_none=True)
     codes_postaux = fields.List(fields.String(), dump_only=True, allow_none=True)
     departement = fields.Nested(DepartementSchema, dump_only=True, allow_none=True)
+    charniere = fields.String(dump_only=True, allow_none=True)
+    article = fields.String(dump_only=True, allow_none=True)
 
     @post_load
     def load_commune(self, data, **kwargs):
