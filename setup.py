ac82ba1578d5815b4d1208953bd6980175edf0fe
Vincent Lara <lara.vincent@gmail.com>
2022-04-27 17:27:55 +0200

    upgrade indice_pollution

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ DEPENDENCIES = [
     'Flask-Manage-Webpack',
     'html2text', # to convert from HTML to markdown, used by one alembic revision
     'wtforms[email]',
-    'indice_pollution>=0.51',
+    'indice_pollution>=0.54',
     'markdown',
     'markdown-link-attr-modifier',
     'openpyxl',
