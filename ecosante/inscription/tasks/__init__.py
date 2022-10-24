1e8ff6717cac1c4e5117b730bd5284dd3cc12b22
Vincent Lara <lara.vincent@gmail.com>
2022-10-24 12:32:14 +0200

    Désactiver les contacts tous les jours

--- ecosante/inscription/tasks/__init__.py
+++ ecosante/inscription/tasks/__init__.py
@@ -1,3 +1,18 @@
+from celery.schedules import crontab
+from ecosante.extensions import celery
+
+
 from .send_success_email import send_success_email #noqa
 from .send_update_profile import send_update_profile #noqa
 from .send_unsubscribe import send_unsubscribe, send_unsubscribe_error #noqa
+from .send_deactivated_contacts import send_deactivated_contacts #noqa
+
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+     sender.add_periodic_task(
+        crontab(hour='04', day_of_week='*/1'),
+        send_deactivated_contacts.s(),
+        queue='send_email',
+        routing_key='send_email.send_deactivated_contacts'
+    )
---
+++ ecosante/inscription/tasks/send_deactivated_contacts.py
@@ -0,0 +1,40 @@
+from datetime import date
+import os
+
+from flask import current_app
+import sib_api_v3_sdk
+
+from ecosante.extensions import celery, db, sib
+from ecosante.inscription.models import Inscription
+
+@celery.task()
+def send_deactivated_contacts():
+    query = Inscription.query.filter_by(
+        Inscription.deactivation_date == date.today(),
+        Inscription.mail.is_not(None)
+    )
+    for inscription in query.all():
+        email_api = sib_api_v3_sdk.TransactionalEmailsApi(sib)
+        try:
+            email_api.send_transac_email(
+                sib_api_v3_sdk.SendSmtpEmail(
+                    sender=sib_api_v3_sdk.SendSmtpEmailSender(
+                        name= "Recosanté",
+                        email= "hi@recosante.beta.gouv.fr"
+                    ),
+                    to=[sib_api_v3_sdk.SendSmtpEmailTo(email=inscription.mail)],
+                    reply_to=sib_api_v3_sdk.SendSmtpEmailReplyTo(
+                        name="Recosanté",
+                        email="hi@recosante.beta.gouv.fr"
+                    ),
+                    template_id=(os.getenv('SIB_DEACTIVATED_CONTACT', 1454))
+                )
+            )
+        except sib.rest.ApiException as e:
+            current_app.logger.error(
+                f"Error: {e}"
+            )
+            raise e
+        inscription.mail = None
+        db.session.add(inscription)
+    db.session.commit()
